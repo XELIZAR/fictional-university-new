e84d11dbcab7d7bcd1f7c722c2e82605b085ef42
xelizar/ <levcikarseny@gmail.com>
2023-06-08 12:59:59 +0200

    Navigation Menus

--- app/public/wp-content/themes/fictional-university-theme/footer.php
+++ app/public/wp-content/themes/fictional-university-theme/footer.php
@@ -13,7 +13,7 @@
                 <div class="site-footer__col-two">
                     <h3 class="headline headline--small">Explore</h3>
                     <nav class="nav-list">
-                        <ul>
+                       <ul>
                             <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                             <li><a href="#">Programs</a></li>
                             <li><a href="#">Events</a></li>
@@ -25,7 +25,7 @@
                 <div class="site-footer__col-three">
                     <h3 class="headline headline--small">Learn</h3>
                     <nav class="nav-list">
-                        <ul>
+                         <ul>
                             <li><a href="#">Legal</a></li>
                             <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
                             <li><a href="#">Careers</a></li>
