be22549211b70604a909593adf186b36a49e6d67
xelizar/ <levcikarseny@gmail.com>
2023-06-09 15:16:16 +0200

    Some Edits

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -16,7 +16,7 @@
         <div class="site-header__menu group">
             <nav class="main-navigation">
                 <ul>
-                    <li <?php if (is_page('about-us') or wp_get_post_parent_id(get_the_ID(0) == 5 )) echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                    <li <?php if (is_page('about-us') or wp_get_post_parent_id(get_the_ID(0) == 5  )) echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                     <li><a href="#">Programs</a></li>
                     <li><a href="#">Events</a></li>
                     <li><a href="#">Campuses</a></li>
