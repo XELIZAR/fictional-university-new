9ba97c8f783e092613fc88c4927c6e828f0c2415
xelizar/ <levcikarseny@gmail.com>
2023-06-11 16:05:24 +0200

    Few Quick Edits & Improvements

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -68,7 +68,7 @@ get_header(); ?>
             ?>
 
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+            <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
     </div>
 </div>
