70d362769eb726689070e0ce38b1af587379bf0e
xelizar/ <levcikarseny@gmail.com>
2023-06-12 19:15:38 +0200

    Custom Post Types

---
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,13 @@
+<?php
+function university_post_types () {
+register_post_type('event', array(
+'public' => true,
+'labels' => array(
+'name' => 'Events',
+    'add_new_item' => 'Add New Event',
+    'edit_item' => 'Edit Event'
+),
+'menu_icon' => 'dashicons-calendar'
+));
+}
+add_action('init', 'university_post_types');
--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -15,3 +15,4 @@ function university_features() {
 }
 
 add_action('after_setup_theme', 'university_features');
+
