6a963b11dabbc4f7fbda49b0bf9728b69f8cbb6a
xelizar/ <levcikarseny@gmail.com>
2023-06-12 19:17:51 +0200

    Custom Post Types

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -5,7 +5,9 @@ register_post_type('event', array(
 'labels' => array(
 'name' => 'Events',
     'add_new_item' => 'Add New Event',
-    'edit_item' => 'Edit Event'
+    'edit_item' => 'Edit Event',
+    'all_items' => 'All Events',
+    'singular_name' => 'Event',
 ),
 'menu_icon' => 'dashicons-calendar'
 ));
